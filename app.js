fa71561e3473942a03ae7fa3fb80cc4075141549
Pedro Díaz <pgdiaz.bautista@gmail.com>
2022-09-17 10:33:18 -0500

    Consola interactiva

---
+++ app.js
@@ -0,0 +1,63 @@
+import color from 'colors'
+
+import { guardarDB, leerDB } from './helpers/guardarArchivo.js';
+import {confirmar, inquirerMenu, leerInput, listadoTareasBorrar, mostrarListadoCheckList, pausa} from './helpers/inquirer.js';
+import {Tareas} from './models/tareas.js';
+
+
+const main = async() => {
+
+    let opt = '';
+    const tareas = new Tareas();
+
+    const tareasDB =  leerDB();
+
+    if (tareasDB) {
+        tareas.cargarTareasFromArray(tareasDB);
+    }
+    
+    do {
+
+        opt = await inquirerMenu();
+
+        switch(opt){
+            case '1':
+                const desc = await leerInput('Descripción:')
+                tareas.crearTarea(desc);
+            break;
+            case '2':
+                tareas.listadoCompleto()
+            break;
+            case '3':
+                tareas.listarPendientesCompletadas(true);
+
+            break;
+            case '4':
+                tareas.listarPendientesCompletadas(false);
+            break;
+            case '5':
+                const ids = await mostrarListadoCheckList(tareas.listadoArr);
+                tareas.toggleCompletadas(ids);
+            break;
+            case'6':
+            const id = await listadoTareasBorrar(tareas.listadoArr);
+            if (id !=='0') {
+                const ok = await confirmar('Estas seguro?'); 
+                if (ok) {
+                    tareas.borrarTarea(id);
+                    console.log('Tarea borrada');
+                }
+            }
+            break;
+        }
+
+        guardarDB(tareas.listadoArr);
+        
+        await pausa();
+
+    } while (opt !== '0');
+}
+ 
+
+main();
+
